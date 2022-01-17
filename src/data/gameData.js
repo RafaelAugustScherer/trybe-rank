9391b0883e6a2e452fa6781b41d461b3332d27c4
Leandro <play3rtox1c@gmail.com>
2022-01-17 09:24:45 -0300

    update: adiciona tabela de type no data test

--- src/data/gameData.js
+++ src/data/gameData.js
@@ -1,22 +1,42 @@
+const type = [
+  {
+    id: 1,
+    name: 'map',
+    difficulty: 2
+  },
+  {
+    id: 2,
+    name: 'reduce',
+    difficulty: 5
+  },
+  {
+    id: 3,
+    name: 'filter',
+    difficulty: 3
+  },
+]
+
 const data = [
   {
-    type: 'map',
+    type: 1,
     question: 'Primeira questão',
     correct_answer: '1',
     wrong_answers: ['2', '3', '4']
   },
   {
-    type: 'map',
+    type: 1,
     question: 'Segunda questão',
     correct_answer: '2',
     wrong_answers: ['1', '3', '4']
   },
   {
-    type: 'map',
+    type: 1,
     question: 'Terceira questão',
     correct_answer: '3',
     wrong_answers: ['2', '1', '4']
-  },
+  }
 ];
 
-export default data;
+const gameData = { data, type }
+
+export default gameData;
