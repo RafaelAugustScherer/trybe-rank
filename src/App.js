2b7057e4c1999363676420ee19d7a9e2d67a3951
Leandro <play3rtox1c@gmail.com>
2022-01-17 07:59:58 -0300

    update: adiciona o provider no SelectQuiz(App.js)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import GameProvider from './providers/GameProvider';
 import SelectQuiz from './pages/Select-quiz';
 import './App.css';
 
@@ -9,7 +10,14 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <Login /> } />
-        <Route path="/select-quiz" element={ <SelectQuiz /> } />
+        <Route 
+          path="/select-quiz" 
+          element={
+            <GameProvider>
+              <SelectQuiz /> 
+            </GameProvider>
+          } 
+        />
       </Routes>
     </BrowserRouter>
   );
