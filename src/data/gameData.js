8f2886f42bd259759f7a4f103a7d5cc64a9edfa3
Leandro <play3rtox1c@gmail.com>
2022-01-17 08:58:58 -0300

    add: data para teste

---
+++ src/data/gameData.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    type: 'map',
+    question: 'Primeira questão',
+    correct_answer: '1',
+    wrong_answers: ['2', '3', '4']
+  },
+  {
+    type: 'map',
+    question: 'Segunda questão',
+    correct_answer: '2',
+    wrong_answers: ['1', '3', '4']
+  },
+  {
+    type: 'map',
+    question: 'Terceira questão',
+    correct_answer: '3',
+    wrong_answers: ['2', '1', '4']
+  },
+];
+
+export default data;
