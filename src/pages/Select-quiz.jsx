d34ed92be671fca28c09caa67ddef47b64c68db8
Leandro <play3rtox1c@gmail.com>
2022-01-18 10:46:34 -0300

    update: adiciona parte de escolher dificuldade

--- src/pages/Select-quiz.jsx
+++ src/pages/Select-quiz.jsx
@@ -1,34 +1,69 @@
 import { useState } from 'react';
 import { types, questions } from '../data/gameData';
 import QuizCard from '../components/quizCard';
+import SelectDificulty from '../components/SelectDificulty';
 import '../css/Select-page.css';
 
 const SelectQuiz = () => {
-  const [selected, setSelected] = useState(null)
+  const [selected, setSelected] = useState(null);
+  const [active, setActive] = useState(null);
 
   const createCards = () => {
     const cards = types.map(({id, name, difficulty, color}) => {
       const quantity = questions.filter(({ type_id }) => id === type_id).length
       return (
-        <QuizCard 
-          id={ id }
-          title={ name }
-          color={ color }
-          quantity={ quantity }
-          dificulty={ difficulty }
-          selected={ selected }
-          setSelected={ setSelected }
-        />
+        <>
+          <div
+            onClick={ () => setSelected(null) }
+            className="backpage"
+          />
+          <QuizCard
+            key={ id }
+            id={ id }
+            title={ `.${name}()` }
+            color={ color }
+            quantity={ quantity }
+            dificulty={ difficulty }
+            selected={ selected }
+            setActive={ setActive }
+            setSelected={ setSelected }
+          />
+        </>
       )
     })
     return cards;
   };
 
+  const renderCardActive = () => {
+    const { id, name, color, difficulty } = types.find(({ id }) => id === active);
+    const quantity = questions.filter((question) => question.type_id === id).length;
+    return (
+      <>
+        <div
+          onClick={ () => setActive(null) }
+          className="backpage"
+        />
+        <QuizCard
+          id={ id }
+          title={ `.${name}()` }
+          color={ color }
+          quantity={ quantity }
+          dificulty={ difficulty }
+          jogar
+          selected={ active }
+          setActive={ setActive }
+          setSelected={ setSelected }
+        />
+        <SelectDificulty />
+      </>
+    )
+  }
+
   return (
     <div className="select-page">
       <h1 className="hero-title">Seleção de Quiz</h1>
       <div className="type-cards-container">
-        { createCards() }
+        { active ? renderCardActive() :createCards() }
       </div>
     </div>
   )
