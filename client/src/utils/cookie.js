4da4e478d7560ea70cb37acb27f2df7377bae50c
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-02-05 15:39:59 -0300

    update: Expirydate agora é UTC - 3 (Brasília)

--- client/src/utils/cookie.js
+++ client/src/utils/cookie.js
@@ -10,7 +10,7 @@ const getCookie = (name) => {
 
 const setCookie = (name, value, expiryInHours) => {
   const expiryDate = new Date();
-  expiryDate.setTime(expiryDate.getTime() + (expiryInHours * 60 * 60 * 1000));
+  expiryDate.setTime(expiryDate.getTime() + ((expiryInHours - 3) * 60 * 60 * 1000));
   const expires = `expires=${expiryDate.toUTCString()}`;
 
   document.cookie = `${name}=${value};${expires};`;
