09f946642f956bdd0943bd634e500a2f0ef4a632
Leandro Henrique <play3rtox1c@gmail.com>
2022-02-06 15:20:41 -0300

    add: pagina de leaderboard e rotas para ela

---
+++ client/src/pages/Leaderboard.jsx
@@ -0,0 +1,134 @@
+import { useContext, useEffect, useState } from "react";
+import { infoContext } from "../providers/InfoProvider";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import '../css/Leaderboard.css'
+
+const Leaderboard = () => {
+  const [type, setType] = useState('All');
+  const [difficulty, setDificulty] = useState('All')
+  const [users, setUsers] = useState([]);
+  const { types, token } = useContext(infoContext);
+
+  const getUsers = async () => {
+    const headers = { 'Authorization': `${token}` }
+
+    const { users: leaderboardUsers } = await axios.get('http://localhost:5000/users', { headers })
+      .then((res) => res.data);
+    
+    setUsers(leaderboardUsers);
+  }
+
+  const getPlayers = () => {
+    const filterByQuizes = users.filter(({ completed_quizes: completedQuizes }) => completedQuizes.length);
+    const getUserAndPontuation = filterByQuizes.map(({ completed_quizes: completedQuizes, nickname }) => {
+      const pontuation = completedQuizes.reduce((acc, curr) => {
+        if (type === 'All' && difficulty === 'All') return acc += curr.score;
+        if (type === 'All' && difficulty === curr.difficulty) return acc += curr.score;
+        if (difficulty === 'All' && type === curr.type) return acc += curr.score;
+        if (difficulty === curr.difficulty && type === curr.type) return acc += curr.score;
+        return acc;
+      }, 0);
+      return { nickname, pontuation }
+    });
+    const getTop10Players = getUserAndPontuation
+      .sort(({ pontuation: a }, { pontuation: b }) => a - b)
+      .slice(0, 10);
+
+    return getTop10Players;
+  }
+
+  const renderOptions = (option) => {
+    let options;
+
+    if (option === 'type') {
+      options = types.map(({ name }) => (
+        <option key={ `option - ${name}` } value={ name }>{ name }</option>
+      ))
+    } else {
+      const difficulties = ['Iniciante', 'Intermediario', 'Dificil']
+      options = difficulties.map((dificulty) => (
+        <option key={ `option - ${dificulty}` } value={ dificulty }>{ dificulty }</option>
+      ))
+    }
+
+    return options;
+  }
+
+  const renderLeaderboard = () => {
+    const players = getPlayers();
+    const rows = players.map(({ nickname, pontuation }, index) => (
+      <tr key={ `jogador - ${nickname} / pontuacao - ${ pontuation }` }>
+        <td>{ index + 1 + 'º' }</td>
+        <td>{ nickname }</td>
+        <td>{ pontuation }</td>
+      </tr>
+    ))
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Top</th>
+            <th>Jogador</th>
+            <th>Pontuação</th>
+          </tr>
+        </thead>
+        <tbody>
+          { rows }
+        </tbody>
+      </table>
+    )
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <>
+      <div className="leaderboard-page">
+        <h1 className="hero-title">
+        { `
+          Leaderboard
+          ${ type !== 'All' ? ` de ${type}` : '' }
+          ${ difficulty !== 'All' ? ` (${difficulty})` : ''}
+        ` }
+        </h1>
+        <div>
+          <div className="leaderbord-container">
+            <h3>Filtros:</h3>
+            <div className="filter-container">
+              <label>
+                Por tipo:
+                <select value={ type } onChange={ ({ target: { value } }) => setType(value) } >
+                  <option value={'All'}>All</option>
+                  { renderOptions('type') }
+                </select>
+              </label>
+              <label>
+                Por dificuldade:
+                <select value={ difficulty } onChange={ ({ target: { value } }) => setDificulty(value) } >
+                  <option value={'All'}>All</option>
+                  { renderOptions('dificulty') }
+                </select>
+              </label>
+            </div>
+            <div className="leaderboard">
+              { !!users.length && renderLeaderboard() }
+            </div>
+          </div>
+          <div className="next-prev-buttons">
+            <Link to="/select-quiz">
+              <button className="voltar-menu">
+                Voltar para o menu
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Leaderboard;
