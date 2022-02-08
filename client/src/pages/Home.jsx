8e9a587b4116ca5d1d431b77952ae0452831bb0b
Leandro Henrique <play3rtox1c@gmail.com>
2022-02-08 18:44:16 -0300

    update: corrigindo erros de merge

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,11 +2,10 @@ import { useState, useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { BsTrophyFill, BsFillPeopleFill, BsFront } from 'react-icons/bs';
 import { infoContext } from '../providers/InfoProvider';
-import { fetchUsers } from '../utils/fetch/users';
+import { getPlayersAround, createTable } from '../utils/leaderboard';
+import Menu from '../components/Menu';
 import ProfileCard from '../components/profileCard';
 import TypeCards from '../components/typeCards';
-import Menu from '../components/Menu';
-import { getPlayersAround, createTable } from '../utils/leaderboard';
 import '../css/home-page.css';
 
 const Home = () => {
@@ -14,30 +13,41 @@ const Home = () => {
   const { token, userInfo } = useContext(infoContext);
   const { nickname } = userInfo;
   const [players, setPlayers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [rank, setRank] = useState(1);
 
   const getPlayers = async () => {
     const newPlayers = await getPlayersAround(token, nickname);
     setPlayers(newPlayers);
+
+    const thisPlayer = newPlayers.find(({nickname: ply3rNickname}) => ply3rNickname === nickname);
+    if (thisPlayer) {
+      const newRank = newPlayers.indexOf(thisPlayer) + 1;
+      console.log(thisPlayer.score, newRank);
+      setScore(thisPlayer.score);
+      setRank(newRank);
+    } 
   };
 
   useEffect(() => {
     token && getPlayers();
-  }, [token]);
+  }, [nickname, token]);
 
   return (
     <div className="container-master">
-      <Menu path="home"/>
+      <Menu path="home" />
       <div className="home-page">
+        <h1 className="hero-title">Home</h1>
         <section
           key="welcome-section"
-          className="welcome"
+          className="welcome home-section"
         >
           <h2>Bem-vindo {nickname}!</h2>
-          <ProfileCard />
+          <ProfileCard score={ score } rank={ rank } />
         </section>
         <section
           key="quick-access-section"
-          className="quick-access"
+          className="home-section quick-access"
         >
           <h2>Acesso Rápido</h2>
           <button onClick={() => navigate('/select-quiz')}>
@@ -53,16 +63,18 @@ const Home = () => {
             <p>Comunidade</p>
           </button>
         </section>
-        <section key="quiz-progress-section">
+        <section
+          key="quiz-progress-section"
+          className="home-section">
           <h2>Progresso nos Quizes</h2>
           <TypeCards isMini />
         </section>
-        <section key="leaderboard-section">
+        <section
+          key="leaderboard-section"
+          className="home-section">
           <h2>Leaderboard</h2>
-          <div className="leaderboard-home">
-            <div className="leaderboard">
-              { !!users.length && renderLeaderboard() }
-            </div>
+          <div className="leaderboard">
+            { !!players.length && createTable(players) }
           </div>
         </section>
       </div>
