c73bc84c4a60272dc276f02c3cdbd10f6cf462ff
Leandro Henrique <play3rtox1c@gmail.com>
2022-02-25 16:45:52 -0300

    update: index.js e package*.json

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-import users from './controllers/users.js';
-import questions from './controllers/questions.js';
-import types from './controllers/types.js';
+import users from './src/routes/users.js';
+import questions from './src/routes/questions.js';
+import types from './src/routes/types.js';
+import error from './src/middlewares/error.js'
 
 const corsOptions = {
   origin: '*',
@@ -14,8 +15,12 @@ const app = express();
 
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+
 app.use(express.json());
 app.use(cors(corsOptions));
-app.use('/', users);
-app.use('/', questions);
-app.use('/', types);
+
+app.use('/user', users);
+app.use('/questions', questions);
+app.use('/types', types);
+
+app.use(error);
