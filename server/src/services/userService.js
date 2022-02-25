fabeb01d9ab78b17389b6252cb7f21f15db8a417
Leandro Henrique <play3rtox1c@gmail.com>
2022-02-25 16:41:13 -0300

    update: refatorando o service

---
+++ server/src/services/userService.js
@@ -0,0 +1,121 @@
+import jwt from 'jsonwebtoken';
+import userModel from '../models/userModel.js';
+import { ObjectId } from 'mongodb';
+import ValidateError from '../validateError.js';
+import connect from '../models/connection.js';
+const { usersCollection } = await connect();
+import dotenv from 'dotenv';
+dotenv.config();
+
+const secret = process.env.TOKEN_KEY;
+const jwtConfig = {
+  expiresIn: '24h',
+  algorithm: 'HS256',
+};
+
+const findAll = async () => {
+  const users = await usersCollection.find().toArray();
+
+  if (!users.length)
+    throw new ValidateError({ status: 404, message: 'Users not found' })
+
+  const userWithOutPass = users.map(({ nickname, completed_quizes }) => ({ nickname, completed_quizes }))
+
+  return userWithOutPass;
+}
+
+const findOne = async (username) => {
+  const result = await usersCollection.findOne({ username });
+
+  if (!result) {
+    throw new ValidateError({ status: 404, message: 'User not found' })
+  }
+
+  const { password, ...fields } = result;
+
+  return fields;
+};
+
+const create = async (params) => {
+  const { username } = params;
+  
+  const alreadyExists = await usersCollection.findOne({ "username": username });
+  if (alreadyExists) {
+    throw new ValidateError({ status: 409, message: 'User already exists' })
+  }
+  
+  const user = new userModel({ ...params, nickname: username });
+  const { insertId } = await usersCollection.insertOne(user);
+
+  return { id: insertId, ...params };
+}
+
+const update = async (params, data) => {
+  const user = await usersCollection.updateOne(
+    { ...params }, 
+    { $set: { ...data } }
+  );
+  
+  return user;
+}
+
+const getCompletedQuestions = (completedQuestions, newQuestions) => {
+  const questionsIds = completedQuestions.map(({ question_id }) => question_id);
+  const filterQuestions = newQuestions
+    .filter(({ question_id }) => !questionsIds.includes(question_id));
+
+  if (!filterQuestions.length)
+    return [];
+
+  const newCompletedQuestions = [...completedQuestions, ...filterQuestions]
+  return newCompletedQuestions;
+}
+
+const updateProgress = async (params) => {
+  const { 
+    username, 
+    completed_questions,
+    completed_quizes,
+    newQuestions,
+    newQuiz 
+  } = params;
+
+  const newCompletedQuestions = getCompletedQuestions(completed_questions, newQuestions)
+  const completedQuiz = { quizId: ObjectId(), ...newQuiz }
+
+  const questions = newCompletedQuestions.length ? newCompletedQuestions : completed_questions
+
+  await update(
+    { username }, 
+    { 
+      completed_questions: questions, 
+      completed_quizes: [...completed_quizes, completedQuiz] 
+    }
+  )
+
+  return { 
+    username, 
+    completed_questions: questions, 
+    completed_quizes: [...completed_quizes, completedQuiz]
+  }
+}
+const auth = async (params) => {
+  const { username, password } = params;
+  const user = await usersCollection.findOne({ username, password })
+
+  if (!user) {
+    throw new ValidateError({ status: 404, message: 'User not found!' })
+  }
+
+  const token = jwt.sign(user, secret, jwtConfig);
+  return token;
+}
+
+export default {
+  findAll, 
+  findOne,
+  create,
+  update,
+  updateProgress,
+  auth
+}
