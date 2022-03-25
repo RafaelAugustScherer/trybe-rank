38b00b429d4f58397d949819675ef0ab30650221
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-03-25 11:12:50 -0300

    Fix: Rank de usuários sem questões completas

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,24 +16,34 @@ const Home = () => {
   const { nickname } = userInfo;
   const [players, setPlayers] = useState([]);
   const [score, setScore] = useState(0);
-  const [rank, setRank] = useState(1);
+  const [rank, setRank] = useState(0);
 
   const getPlayers = async () => {
     const newPlayers = await getPlayersAround(token, nickname);
     setPlayers(newPlayers);
+  };
 
-    const thisPlayer = newPlayers.find(({nickname: ply3rNickname}) => ply3rNickname === nickname);
+  const getRank = () => {
+    const thisPlayer = players.find(({nickname: ply3rNickname}) => ply3rNickname === nickname);
+    
     if (thisPlayer) {
-      const newRank = newPlayers.indexOf(thisPlayer) + 1;
-      
+      const newRank = players.indexOf(thisPlayer) + 1;
+
       setScore(thisPlayer.score);
       setRank(newRank);
-    } 
-  };
+    } else {
+      setPlayers([...players, { nickname, score }]);
+      setRank(players.length);
+    }
+  }
 
   useEffect(() => {
     token && getPlayers();
   }, [nickname, token]);
+
+  useEffect(() => {
+    getRank();
+  }, [players]);
 
   const renderPage = () => (
     <>
@@ -44,7 +54,7 @@ const Home = () => {
           className="home-section welcome"
         >
           <h2>Bem-vindo {nickname}!</h2>
-          <ProfileCard score={ score } rank={ rank } />
+          <ProfileCard score={ score } rank={ rank !== 0 ? rank : '' } />
         </section>
         <section
           key="quick-access-section"
